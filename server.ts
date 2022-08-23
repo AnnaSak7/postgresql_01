6ab439e9a72d2bb83f5e425e406efcd8e9c6fb03
Anna Sakurai <zenasak@gmail.com>
2022-08-23 19:25:43 +0200

    Create api to get all user info using pq module.

--- server.ts
+++ server.ts
@@ -1,11 +1,18 @@
 import express, { application } from "express";
+import pool from "./db";
 
 const app = express();
 
 const PORT = 5555;
 
-// app.get("/", (req, res) => {
-//   res.send("Hello Express");
-// });
+app.use(express.json());
+
+// get all user data
+app.get("/users", (req, res) => {
+  pool.query("SELECT * FROM users", (error, results) => {
+    if (error) throw error;
+    return res.status(200).json(results.rows); //when you use json format, you need to set the middleware (line 8)
+  });
+});
 
 app.listen(PORT, () => console.log(`Server running @ ${PORT}`));
